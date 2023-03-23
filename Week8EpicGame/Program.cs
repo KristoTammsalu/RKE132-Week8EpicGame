d876bae9ee73f0d634ebe20d0e0ffa7f4f9b89ac
Kristi Tammsalu <kristi.tammsalu@metalexpress.local>
2023-03-23 22:44:24 +0200

    create new app

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,35 @@
+﻿string folderPath = @"C:\\Users\\kristi.tammsalu\\Program\\Programmeerimine";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+
+//string[] heroes = { "Harry Potter", "Ratatouille", "Shrek", "Jack Sparrow" };
+//string[] villains = { "Mr. Burns", "Joker", "Megamind", "Voldemort", "Dracula" };
+
+
+
+string[] weapon = { "random stick", "a spoon", "a cat", "small rock", "water gun" };
+
+
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int RandomIndex = rnd.Next(0, someArray.Length);
+    string ranomStringFromArray = someArray[RandomIndex];
+    return ranomStringFromArray;
+}
